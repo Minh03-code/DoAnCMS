48782a0aeaec308f1f1293dfebcd5df947572861
nguyenthihong190402 <105511378+nguyenthihong190402@users.noreply.github.com>
2023-05-18 23:02:40 +0700

    2023/05/18 update module 2

--- wordpress/wp-content/themes/jobscout/job_manager/content-job_listing.php
+++ wordpress/wp-content/themes/jobscout/job_manager/content-job_listing.php
@@ -24,51 +24,64 @@ $company_name = get_post_meta(get_the_ID(), '_company_name', true);
 
 ?>
 <div class="col-6">
-	<article <?php job_listing_class(); ?> data-longitude="<?php echo esc_attr($post->geolocation_lat); ?>" data-latitude="<?php echo esc_attr($post->geolocation_long); ?>">
+	<div class="bg-white">
+		<div class="row">
+			<div class="col-md-4">
+				<?php the_company_logo('thumbnail'); ?>
+			</div>
+			<div class="col-md-8 alie">
+				<div class="row">
+					<div class="col-md-12">
+						<h5 class="job-title-module24">
+							<a href="<?php the_job_permalink(); ?>"><?php wpjm_the_job_title(); ?></a>
+						</h5>
 
-		<figure class="company-logo">
-			<?php the_company_logo('thumbnail'); ?>
-		</figure>
+					</div>
+					<div class="col-md-12">
+						<div class="date-job-module24">
+							<?php echo "Create: " . get_the_date('M j, Y'); ?>
+						</div>
+					</div>
+					<div class="col-md-12">
+						<div class="bg-gray-module-24">
+							<div class="row">
+								<div class="col-md-4 border-right text-company">
+									<?php
+									if (get_option('job_manager_enable_types')) {
+										$types = wpjm_get_the_job_types();
+										if (!empty($types)) : foreach ($types as $jobtype) : ?>
+												<?php echo esc_html($jobtype->name); ?>
+									<?php endforeach;
+										endif;
+									}
+									do_action('job_listing_meta_end');
+									?>
 
-		<div class="job-title-wrapper">
-			<h2 class="entry-title-company">
-				<a href="<?php the_job_permalink(); ?>"><?php wpjm_the_job_title(); ?></a>
-			</h2>
-
-			<span class="date-post"><?php echo "Create: " . get_the_date('M j, Y'); ?></span>
-
-			<div class="row bg">
-				<div class="company1">
-					<?php
-					if (get_option('job_manager_enable_types')) {
-						$types = wpjm_get_the_job_types();
-						if (!empty($types)) : foreach ($types as $jobtype) : ?>
-								<li class="job-type-company <?php echo esc_attr(sanitize_title($jobtype->slug)); ?>"><?php echo esc_html($jobtype->name); ?></li>
-					<?php endforeach;
-						endif;
-					}
-					do_action('job_listing_meta_end');
-					?>
+								</div>
+								<div class="col-md-4 border-right text-company">
+									<?php the_company_name('<strong>', '</strong> '); ?>
+								</div>
+								<div class="col-md-4 text-company text-company">
+									<?php the_job_location(true); ?>
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
-
-				<div class="company2">
-					<?php the_company_name('<strong>', '</strong> '); ?>
-				</div>
-
-				<div class="company-location">
-					<?php the_job_location(true); ?>
+			</div>
+			<div class="col-md-12">
+				<div class="row">
+					<div class="col-md-12 description">
+						<?php echo wp_trim_words(get_the_content(), 20, ''); ?>
+						<a href="<?php echo get_permalink() ?>">[...]</a>
+					</div>
+					<div class="col-md-12">
+						<?php if ($job_featured) { ?>
+							<div class="featured-label"><?php esc_html_e('Featured', 'jobscout'); ?></div>
+						<?php } ?>
+					</div>
 				</div>
 			</div>
 		</div>
-
-		<div class="job-description-content">
-			<?php echo wp_trim_words(get_the_content(), 25, ''); ?>
-			<a href="<?php echo get_permalink() ?>">[...]</a>
-		</div>
-
-		<?php if ($job_featured) { ?>
-			<div class="featured-label"><?php esc_html_e('Featured', 'jobscout'); ?></div>
-		<?php } ?>
-
-	</article>
+	</div>
 </div>
